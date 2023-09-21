81a06eb3fb8d19c4c8ba596d364f73bec81f75ad
Hector <hector.suarez@upc.edu>
2023-09-21 10:53:15 +0200

    Seccion de comentarios añadida

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
@@ -8,6 +9,9 @@ urlpatterns = [
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('post/new/', views.post_new, name='post_new'),
-    path('drafts/', views.post_draft_list, name='post_draft_list')
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
 
